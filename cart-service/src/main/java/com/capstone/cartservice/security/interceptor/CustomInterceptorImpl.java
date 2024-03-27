84b06aa8f3043063663173d23f12aaf3e7c81d33
arnav33 <this.arnav@gmail.com>
2024-03-27 11:25:31 +0530

    authorization implemented on cart service

---
+++ cart-service/src/main/java/com/capstone/cartservice/security/interceptor/CustomInterceptorImpl.java
@@ -0,0 +1,34 @@
+package com.capstone.cartservice.security.interceptor;
+
+import com.capstone.cartservice.security.services.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.naming.AuthenticationException;
+
+public class CustomInterceptorImpl implements HandlerInterceptor {
+
+    AuthService authService;
+
+    public CustomInterceptorImpl(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler
+    ) throws Exception {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null) {
+            throw new AuthenticationException("Access Denied!");
+        }
+        String status = this.authService.validate(authorizationHeader);
+        if(!status.equals("ACTIVE")) {
+            throw new AuthenticationException("Access denied");
+        }
+        return true;
+    }
+}
