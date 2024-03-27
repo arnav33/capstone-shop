23c04e7568f109decff7befc48164dcccbf37f99
arnav33 <this.arnav@gmail.com>
2024-03-27 13:27:03 +0530

    code cleanup. removed unused imports

--- order-management-ervice/src/main/java/com/capstone/ordermanagementervice/dtos/CreateOrderRequest.java
+++ order-management-ervice/src/main/java/com/capstone/ordermanagementervice/dtos/CreateOrderRequest.java
@@ -1,7 +1,5 @@
 package com.capstone.ordermanagementervice.dtos;
 
-import com.capstone.ordermanagementervice.enumerations.PayType;
-import com.capstone.ordermanagementervice.enumerations.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
