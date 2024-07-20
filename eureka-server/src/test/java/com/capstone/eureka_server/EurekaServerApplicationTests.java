f0ff72476d86f2e6a182a12b9cbba1f9cd68a80a
arnav33 <this.arnav@gmail.com>
2024-07-20 14:27:38 +0530

    [eureca-server] first commit

---
+++ eureka-server/src/test/java/com/capstone/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
